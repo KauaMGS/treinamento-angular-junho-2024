0c5e6a922ccd3b04137545769baa6a78acbe084c
KauaMGS <kauamoreira90@gmail.com>
2024-06-29 15:20:05 -0300

    Atualizacao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ProfileListComponent } from './pages/profile/profile-list/profile-list.component';
-import { ProfileCreateUpdateComponent } from './pages/profile/profile-create-update/profile-create-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    ProfileListComponent,
-    ProfileCreateUpdateComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'my-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
-  @Input() items: any[] = [];
 
 }
